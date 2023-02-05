369bbf8ac8dc439a4612fe7bbdd1c069081d560a
Siak Hooi <siakhooi@gmail.com>
2023-02-05 16:38:27 +0800

    add tsa

--- .github/workflows/deploy_jar.sh
+++ .github/workflows/deploy_jar.sh
@@ -9,6 +9,33 @@ CODESIGN_ALIAS=g5-java-application-framework
 STOREFILE=$(mktemp --suffix=.pkcs)
 echo $CODESIGN_STORE | sed 's/ //g' | base64 -d >$STOREFILE
 
+TEMPDIR=$(mktemp -d)
+(
+  cd $TEMPDIR
+  curl -LO https://www.freetsa.org/files/tsa.crt
+  curl -LO https://www.freetsa.org/files/cacert.pem
+)
+
+keytool -importcert \
+  -file "$TEMPDIR/tsa.crt" \
+  -keystore "$STOREFILE" \
+  -storepass "$CODESIGN_STORE_PASSWORD" \
+  -noprompt \
+  -trustcacerts \
+  -alias tsa.crt
+
+keytool -importcert \
+  -file "$TEMPDIR/cacert.pem" \
+  -storepass "$CODESIGN_STORE_PASSWORD" \
+  -storepass "$CODESIGN_STORE_PASSWORD" \
+  -noprompt \
+  -trustcacerts \
+  -alias tsa_ca_cert
+
 for f in ./static/lib/*.jar; do
-  jarsigner -keystore "$STOREFILE" -storepass "$CODESIGN_STORE_PASSWORD" "$f" "$CODESIGN_ALIAS"
+  jarsigner -keystore "$STOREFILE" \
+    -storepass "$CODESIGN_STORE_PASSWORD" \
+    -tsa https://freetsa.org/tsr \
+    -tsacert tsa.crt \
+    "$f" "$CODESIGN_ALIAS"
 done
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Please refer <https://siakhooi.github.io/java-g5-framework>
 
 ![GitHub](https://img.shields.io/github/license/siakhooi/java-g5-framework)
+
+## SSH Key Issues
+
+```bash
+ssh-keygen -f g5id -t ed25519 -C "siakhooi@gmail.com"
+```
