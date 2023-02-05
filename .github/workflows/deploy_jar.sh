28e4781dbd860934fc0c3f1e33427d27ba91e4bc
Siak Hooi <siakhooi@gmail.com>
2023-02-05 14:07:42 +0800

    update codesign store source

--- .github/workflows/deploy_jar.sh
+++ .github/workflows/deploy_jar.sh
@@ -1,12 +1,14 @@
-
 mkdir -p static/lib
 
 cp ant.dist/*.jar static/lib/
 
 cp lib.unsigned/*.jar static/lib/
 
-keytool -genkeypair -v -storetype JKS -keystore myKeystore -alias myself -validity 360 -keyalg RSA -dname "cn=Subang Jaya, ou=GQR Solutions, o=GQR Solutions, c=MY" -storepass welcome
+CODESIGN_ALIAS=g5-java-application-framework
+
+STOREFILE=$(mktemp --suffix=.pkcs)
+echo $CODESIGN_STORE | sed 's/ //g' | base64 -d >$STOREFILE
 
 for f in ./static/lib/*.jar; do
-  jarsigner -keystore myKeystore -storepass welcome "$f" myself
+  jarsigner -keystore "$STOREFILE" -storepass "$CODESIGN_STORE_PASSWORD" "$f" "$CODESIGN_ALIAS"
 done
